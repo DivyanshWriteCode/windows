3706858dc7d4df5c199e0a29d5a750edeb0fb826
DivyanshWriteCode <divyanshdocode@gmail.com>
2024-09-15 11:37:37 +0530

    ok

--- src/Component/ProductData.js
+++ src/Component/ProductData.js
@@ -6,7 +6,6 @@ const ProductData = [
     { Title: 'Skill Set 4', Color: '#FF5733',BackGroundColor:'#dbb701', Width: '200px', Height: '250px', Description: 'This Is A Description' },
     { Title: 'Skill Set 5', Color: '#33C4FF', Width: '350px', Height: '250px', BackGroundColor:'#02b9e3' },
 
-    // Add more products with color hex codes, width, and height
 ];
 
 export default ProductData;
